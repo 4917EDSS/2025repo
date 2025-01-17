b3b68f5cb4d8676a232a13d9cee8138e701eaaf3
4917 Wheatley <software@4917.ca>
2025-01-17 16:26:47 -0500

    Added to ClimbSub diagnostics

--- 2025CompetitionBot/src/main/java/frc/robot/commands/tests/TestClimbSub.java
+++ 2025CompetitionBot/src/main/java/frc/robot/commands/tests/TestClimbSub.java
@@ -4,10 +4,14 @@
 
 package frc.robot.commands.tests;
 
+import java.time.Duration;
 import java.time.Instant;
+import com.ctre.phoenix6.StatusCode;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
 import frc.robot.subsystems.ClimbSub;
 import frc.robot.utils.TestManager;
+import frc.robot.utils.TestManager.Result;
 
 /*
  * You should consider using the more terse Command factories API instead
@@ -37,15 +41,52 @@ public class TestClimbSub extends Command {
     // Reset the encoder and run the motor for a given time
     m_startTime = Instant.now();
     m_climbSub.resetPosition();
+    m_climbSub.moveClimb(Constants.Tests.kDriveMotorPower);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    // Nothing to do here but wait for the test to be over
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    if(m_abortTest) {
+      // Already stopped the motor and reported the test results, just quit
+      return;
+    }
+
+    if(interrupted) {
+      m_climbSub.moveClimb(0.0);
+      m_testManager.updateTestStatus(m_testId, Result.kFail, "Test interrupted");
+      return;
+    }
+
+    // Before turning off the motor, read the current current (amps) and position
+    double currentAmps = m_climbSub.getAmps();
+    double currentPosition = m_climbSub.getDistance();
+
+    // Stop the motor
+    m_climbSub.moveClimb(0.0);
+
+    // Check to see if the measured current is good, ok or bad
+    TestManager.Result ampResult = m_testManager.determineResult(currentAmps, Constants.Tests.kDriveMotorExpectedAmps,
+        Constants.Tests.kDriveMotorAmpsTolerance, Constants.Tests.kDriveMotorAmpsMinimum);
+    String ampsText = "Amps=" + currentAmps + " (Target=" + Constants.Tests.kDriveMotorExpectedAmps + "+/-"
+        + Constants.Tests.kDriveMotorAmpsTolerance + ")";
+    System.out.println("ClimbSub " + ampsText);
+
+    // Check to see if the measured position is good, ok or bad
+    TestManager.Result positionResult =
+        m_testManager.determineResult(currentPosition, Constants.Tests.kDriveMotorExpectedPosition,
+            Constants.Tests.kDriveMotorPositionTolerance, Constants.Tests.kDriveMotorPositionMinimum);
+    String positionText =
+        "Position=" + currentPosition + " (Target=" + Constants.Tests.kDriveMotorExpectedPosition + "+/-"
+            + Constants.Tests.kDriveMotorPositionTolerance + ")";
+    System.out.println("ClimbSub " + positionText);
+  }
 
   // Returns true when the command should end.
   @Override
--- 2025CompetitionBot/src/main/java/frc/robot/subsystems/ClimbSub.java
+++ 2025CompetitionBot/src/main/java/frc/robot/subsystems/ClimbSub.java
@@ -4,6 +4,12 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
@@ -14,25 +20,29 @@ import frc.robot.Constants;
 
 public class ClimbSub extends SubsystemBase {
   // Create the climb motor
-  private final SparkMax m_climbMotor = new SparkMax(Constants.CanIds.kClimbMotor, MotorType.kBrushless);
+  private final TalonFX m_climbMotor = new TalonFX(Constants.CanIds.kClimbMotor);
 
   /** Creates a new ClimbSub. */
   public ClimbSub() {
-    SparkMaxConfig config = new SparkMaxConfig();
+    TalonFXConfigurator talonFxConfiguarator = m_climbMotor.getConfigurator();
 
-    config
-        .inverted(true) // Set to true to invert the forward motor direction
-        .smartCurrentLimit(5) // Current limit in amps
-        .idleMode(IdleMode.kBrake); // Set to kCoast to allow the motor to coast when power is 0.0
+    // This is how you set a current limit inside the motor (vs on the input power supply)
+    CurrentLimitsConfigs limitConfigs = new CurrentLimitsConfigs();
+    limitConfigs.StatorCurrentLimit = 40; // Limit in Amps
+    limitConfigs.StatorCurrentLimitEnable = true;
+    talonFxConfiguarator.apply(limitConfigs);
 
-    // Save the configuration to the motor
-    // Only persist parameters when configuring the motors on start up as this operation can be slow
-    m_climbMotor.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+    // This is how you can set a deadband, invert the motor rotoation and set brake/coast
+    MotorOutputConfigs outputConfigs = new MotorOutputConfigs();
+    outputConfigs.DutyCycleNeutralDeadband = 0.02; // Ignore values below 2%
+    outputConfigs.Inverted = InvertedValue.CounterClockwise_Positive; // Invert = Clockwise
+    outputConfigs.NeutralMode = NeutralModeValue.Brake;
+    talonFxConfiguarator.apply(outputConfigs);
 
-    // If we change the parameters (e.g. brake mode) during robot operation, 
-    // we should not save the changes to flash (i.e. want kNoPersistParameters)
-    //m_intakeMotor.configure(config, SparkBase.ResetMode.kNoResetSafeParameters, 
-    //  SparkBase.PersistMode.kNoPersistParameters);
+    // To configure a second motor to follow the first motor
+    //boolean turnOppositeDirectionFromMaster = true; // False if both motors turn in same direction, true to make them turn in opposite directions
+    //m_testMotor2.setControl(new Follower(m_testMotor.getDeviceID(), turnOppositeDirectionFromMaster));
+
     resetEncoder();
   }
 
@@ -47,11 +57,11 @@ public class ClimbSub extends SubsystemBase {
   }
 
   public void resetEncoder() {
-    m_climbMotor.getEncoder().setPosition(0);
+    m_climbMotor.setPosition(0);
   }
 
   public double getDistance() {
-    return m_climbMotor.getEncoder().getPosition();
+    return m_climbMotor.getPosition().getValueAsDouble();
   }
 
   public void resetPosition() {
@@ -59,6 +69,10 @@ public class ClimbSub extends SubsystemBase {
   }
 
   public double getVelocity() {
-    return m_climbMotor.getEncoder().getVelocity();
+    return m_climbMotor.getRotorVelocity().getValueAsDouble();
+  }
+
+  public double getAmps() {
+    return m_climbMotor.getStatorCurrent().getValueAsDouble();
   }
 }
