9573fc1d4d779d6f456be1f0cd03bd327bbfd3ec
4917-3 <software@4917.ca>
2025-02-10 17:38:59 -0500

    Update L2PlacementGrp.java

--- 2025CompetitionBot/src/main/java/frc/robot/commands/L2PlacementGrp.java
+++ 2025CompetitionBot/src/main/java/frc/robot/commands/L2PlacementGrp.java
@@ -5,15 +5,26 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
+import frc.robot.subsystems.ArmSub;
+import frc.robot.subsystems.ElevatorSub;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// NOTE: Consider using this command inline, rather than writing a subclass. For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class L2PlacementGrp extends SequentialCommandGroup {
   /** Creates a new L2PlacementGrp. */
-  public L2PlacementGrp() {
+  public L2PlacementGrp(CommandPS4Controller driverController, ArmSub armSub, ElevatorSub elevatorSub) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands();
+    addCommands(
+        new SetElevatorToHeightCmd(500, elevatorSub),
+        new SetArmToPositionCmd(140, armSub));
+
   }
 }
