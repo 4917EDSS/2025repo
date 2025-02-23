3f6ede0843b7df8219f138dcdc3a5ebec5921bc7
Sam Maier <sam@sammaier.ca>
2025-02-22 22:00:08 -0500

    Update TunerConstantsCompetition.java

--- 2025CompetitionBot/src/main/java/frc/robot/generated/TunerConstantsCompetition.java
+++ 2025CompetitionBot/src/main/java/frc/robot/generated/TunerConstantsCompetition.java
@@ -30,8 +30,9 @@ public class TunerConstantsCompetition {
   // When using closed-loop control, the drive motor uses the control
   // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
   private static final Slot0Configs driveGains = new Slot0Configs()
-      .withKP(0.1).withKI(0).withKD(0)
-      .withKS(0).withKV(0.124);
+      .withKP(0.2).withKI(0).withKD(0)
+      .withKS(0.3).withKV(0.115);
+  
 
   // The closed-loop output type to use for the steer motors;
   // This affects the PID/FF gains for the steer motors
