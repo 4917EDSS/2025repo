9ba2690eeeb2b8c99f770a87a8d8703d8ff926e3
4917-Stronghold <software@4917.ca>
2025-03-08 10:15:48 -0500

    reversed out/in

--- 2025CompetitionBot/src/main/java/frc/robot/commands/ClimbDeployCmd.java
+++ 2025CompetitionBot/src/main/java/frc/robot/commands/ClimbDeployCmd.java
@@ -39,6 +39,6 @@ public class ClimbDeployCmd extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return m_climbSub.isAtInLimit();
+    return m_climbSub.isAtOutLimit();
   }
 }
--- 2025CompetitionBot/src/main/java/frc/robot/commands/ClimbRetractCmd.java
+++ 2025CompetitionBot/src/main/java/frc/robot/commands/ClimbRetractCmd.java
@@ -39,6 +39,6 @@ public class ClimbRetractCmd extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return m_climbSub.isAtOutLimit();
+    return m_climbSub.isAtInLimit();
   }
 }
